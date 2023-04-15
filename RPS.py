763a139dc41f6d247e258029b0a089442bb78d5d
Valentin Lopez <valentinlopezandres@gmail.com>
2023-04-15 18:29:14 +0200

    Long term memory (6 iterations back) strategy. Reward and penalty approach ereased for bad performance. Strategy effective for quincy, kris and mrugesh. Not efecctive enough for abbey

--- RPS.py
+++ RPS.py
@@ -1,50 +1,28 @@
 import random
 import numpy as np
 
-def player(prev_play, opponent_history=[], guessing_history=[], scoreTable=np.zeros((3, 3, 3))):
+def player(prev_play, opponent_history=[], guessing_history=[], scoreTable=np.zeros((3, 3, 3, 3, 3, 3))):
     if prev_play == '':
         opponent_history.clear()
     else:
         opponent_history.append(prev_play)
-    #
-    #                     / S
-    #                    / P
-    #                   / R
-    #       R   P   S  / 
-    #   R |   |   |   |
-    #   P |   |   |   |
-    #   S |   |   |   |
-    #
-    remember = 15
-    reward = 0
-    penalty = 0
 
     responses = {'P': 'S', 'R': 'P', 'S': 'R'}
     choices = ['R', 'P', 'S']
 
-    if len(opponent_history) < 4 : 
+    remember = len(scoreTable.shape)   # How far back in time goes the recall 
+
+    if len(opponent_history) < remember : 
         guess = random.choice(choices)
         guessing_history.append(guess)
         return guess
 
-    lastPlays = [   choices.index(opponent_history[-3]), 
-                    choices.index(opponent_history[-2]), 
-                    choices.index(opponent_history[-1])]
-
-    m = len(lastPlays)
-    back = 0
-    while((m < remember) and (m < len(opponent_history))):
-        m += 1
-        back += 1
-        lastPlays.insert(0,choices.index(opponent_history[-m]))
-
-        if guessing_history[-back] == opponent_history[-back]:
-            scoreTable[lastPlays[0], lastPlays[1], lastPlays[2]] += reward
-        else:
-            scoreTable[lastPlays[0], lastPlays[1], lastPlays[2]] += penalty
+    lastPlays = []
+    for back in range(1, remember+1):
+        lastPlays.insert(0,choices.index(opponent_history[-back]))
     
-    scoreTable[lastPlays[-3], lastPlays[-2], lastPlays[-1]] += 1
+    scoreTable[lastPlays[-6], lastPlays[-5], lastPlays[-4], lastPlays[-3], lastPlays[-2], lastPlays[-1]] += 1
 
-    guess = choices[np.argmax(scoreTable[lastPlays[-2], lastPlays[-1], :])]
+    guess = choices[np.argmax(scoreTable[lastPlays[-5], lastPlays[-4], lastPlays[-3], lastPlays[-2], lastPlays[-1], :])]
     guessing_history.append(guess)
     return responses[guess]
