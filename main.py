eea7d78cf3d4c8e1872fb50236e85781860f4661
Valentin Lopez <valentinlopezandres@gmail.com>
2023-04-15 20:42:57 +0200

    Switch to test mode and final version

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ from RPS_game import play, mrugesh, abbey, quincy, kris, human, random_player
 from RPS import player
 from unittest import main
 
-play(player, quincy, 1000)
-print('')
-play(player, abbey, 1000)
-print('')
-play(player, kris, 1000)
-print('')
-play(player, mrugesh, 1000)
-print('')
+# play(player, quincy, 1000)
+# print('')
+# play(player, abbey, 1000)
+# print('')
+# play(player, kris, 1000)
+# print('')
+# play(player, mrugesh, 1000)
+# print('')
 
 # Uncomment line below to play interactively against a bot:
 # play(human, abbey, 20, verbose=True)
@@ -21,4 +21,4 @@ print('')
 
 
 # Uncomment line below to run unit tests automatically
-#main(module='test_module', exit=False)
+main(module='test_module', exit=False)
