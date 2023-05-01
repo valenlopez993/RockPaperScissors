924e707f5e1ba458e913329bfd98a49f67739a9c
Valentin Lopez <82229989+valenlopez993@users.noreply.github.com>
2023-05-01 20:25:38 +0200

    Update main.py
    Update prints on main to better understand the outputs

--- main.py
+++ main.py
@@ -3,14 +3,19 @@ from RPS_game import play, mrugesh, abbey, quincy, kris, human, random_player
 from RPS import player
 from unittest import main
 
-# play(player, quincy, 1000)
-# print('')
-# play(player, abbey, 1000)
-# print('')
-# play(player, kris, 1000)
-# print('')
-# play(player, mrugesh, 1000)
-# print('')
+print("\n=========================================================")
+print("========== Player 1 is always the bot I built ===========")
+print("=========================================================")
+
+print("\n================== Game against quincy ==================\n")
+play(player, quincy, 1000)
+print("\n================== Game against abbey ===================\n")
+play(player, abbey, 1000)
+print("\n================== Game against kris ====================\n")
+play(player, kris, 1000)
+print("\n================= Game against mrugesh ==================\n")
+play(player, mrugesh, 1000)
+print("\n=========================================================\n")
 
 # Uncomment line below to play interactively against a bot:
 # play(human, abbey, 20, verbose=True)
@@ -21,4 +26,4 @@ from unittest import main
 
 
 # Uncomment line below to run unit tests automatically
-main(module='test_module', exit=False)
+# main(module='test_module', exit=False)
